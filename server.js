d053ad85a9fa0a6ad2fd296ba658af1ad9bf408c
Jeff Zapotoczny <jeff.zapotoczny@sonarsource.com>
2021-03-09 17:06:50 -0500

    Cleanup

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get("/api/hello", (req, res) => {
   res.send({ express: "Hello From Express" });
 });
 
-/* 
+/*
 app.post("/api/world", (req, res) => {
   console.log(req.body);
   res.send("You sent:" + req.body.post);
